2ee15765708663764270028c5a83c44513c22389
Techgirl009 <89428705+Techgirl009@users.noreply.github.com>
2025-07-29 12:17:15 -0400

    Add files via upload

--- object_tracking.py
+++ object_tracking.py
@@ -2,6 +2,12 @@ import cv2
 import time
 import math
 
+p1 = 530
+p2 = 300
+
+xs = []
+ys = []
+
 video = cv2.VideoCapture("bb3.mp4")
 
 # Load tracker 
@@ -27,6 +33,22 @@ def drawBox(img, bbox):
 
 def goal_track(img, bbox):
     x, y,w, h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    c1 = x + int(w/2)
+    c2 = y + int(w/2)
+    cv2.circle(img, (c1,c2), 2, (255,0,0), 5)
+    cv2.circle(img, (int(p1), int(p2)), 2, (0,255,0), 5)
+
+    distance = math.sqrt(((c1-p1)**2)+((c2-p2)**2))
+    print(distance)
+
+    if(distance<=20):
+        cv2.puttext(img, "goal", (300,900), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,255,0), 2)
+    
+    xs.append(c1)
+    ys.append(c2)
+
+    for i in range(len(xs)-1):
+        cv2.circle(img, (xs[i], ys[i]), 2, (0,0,255))
 
 while True:
     
@@ -40,6 +62,7 @@ while True:
     else:
         cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
 
+    goal_track(img, bbox)
     cv2.imshow("result", img)
             
     key = cv2.waitKey(25)
@@ -49,5 +72,3 @@ while True:
 
 video.release()
 cv2.destroyALLwindows()
-
-
